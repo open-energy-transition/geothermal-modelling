8c5bf4ff0fa216ac0d099aae449ba8cd3a2ce1d0
Fabrizio Finozzi <fabrizio.finozzi.business@gmail.com>
2024-09-28 13:03:16 +0200

    code: add crossings comparison

--- analysis/scripts/network_comparison.py
+++ analysis/scripts/network_comparison.py
@@ -1,5 +1,40 @@
+# coding=utf-8
+
+##################################################
+#                                                #
+# Author: Fabrizio Finozzi	                     #
+# Email: fabrizio.finozzi.business@gmail.com     #
+# Version: 0.1                                   #
+# Date: 23.07.2024                               #
+#                                                #
+# Created for Open Energy Transition GmbH        #
+#                                                #
+##################################################
+
+#########################################################################
+#                                                                       #
+# IMPORTANT                                                             #
+#                                                                       #
+# This software is distributed without any warranty.                    #
+#                                                                       #
+# Neither the author nor Open Energy Transition GmbH                    #
+# are liable for any damage caused directly or indirectly by the use    #
+# or misuse of this software.                                           #
+#                                                                       #
+#########################################################################
+
+#############################################################
+#                                                           #
+# CHANGELOG                                                 #
+#                                                           #
+# 0.1 - In progress                                         #
+#                                                           #
+#############################################################
+
+import argparse
 import pathlib
 import geopandas as gpd
+import pandas as pd
 import matplotlib.pyplot as plt
 import pypsa
 import cartopy.crs as ccrs
@@ -51,6 +86,13 @@ def plot_intersection(pypsa_df, eia_df, voltage_class, fig_name):
     fig.savefig(fig_name)
 
 
+# To perform the network topology comparison, execute python network_comparison --plot_network_topology
+# To perform the network topology comparison, execute python network_comparison --plot_network_crossings
+# To perform both actions, execute python network_comparison --plot_network_topology --plot_network_crossings
+parser = argparse.ArgumentParser()
+parser.add_argument("--plot_network_topology", help="Boolean: plot the network topology", action="store_true")
+parser.add_argument("--plot_network_crossings", help="Boolean: plot the network crossings", action="store_true")
+args = parser.parse_args()
 
 # Initial configurations
 eia_name = "EIA"
@@ -64,7 +106,7 @@ pypsa_earth_path = pathlib.Path(base_path, "workflow", "pypsa-earth")
 base_network_pypsa_earth_path = pathlib.Path(pypsa_earth_path, "networks", "US_2021", "base.nc")
 base_network_pypsa_usa_path = pathlib.Path(base_path.parent, "pypsa-usa", "workflow", "resources", "Default", "usa", "elec_base_network.nc")
 eia_base_network_path = pathlib.Path(base_path.parent, "US_Electric_Power_Transmission_Lines_5037807202786552385.geojson")
-gadm_shapes_path = pathlib.Path(base_path.parent, "analysis", "data", "gadm41_USA_1.json")
+gadm_shapes_path = pathlib.Path(base_path, "analysis", "data", "gadm41_USA_1.json")
 
 # Load data
 base_network_pypsa_earth = pypsa.Network(base_network_pypsa_earth_path)
@@ -118,11 +160,12 @@ base_network_pypsa_earth.lines["v_nom_class"] = base_network_pypsa_earth.lines["
 
 eia_voltage_classes = list(eia_base_network["VOLT_CLASS"].unique())
 
-for selected_voltage_class in eia_voltage_classes:
-    fig_name_map = pathlib.Path(plot_dir_path, "network_comparison_pearth_for_voltage_class_{}.png".format(str(selected_voltage_class)))
-    plot_network_comparison(base_network_pypsa_earth, eia_base_network, selected_voltage_class, "PyPSA-Earth base network", fig_name_map)
-    fig_name_intersection = pathlib.Path(plot_dir_path, "network_comparison_intersection_{}.png".format(str(selected_voltage_class)))
-    plot_intersection(base_network_pypsa_earth, eia_base_network, selected_voltage_class, fig_name_intersection)
+if args.plot_network_topology:
+    for selected_voltage_class in eia_voltage_classes:
+        fig_name_map = pathlib.Path(plot_dir_path, "network_comparison_pearth_for_voltage_class_{}.png".format(str(selected_voltage_class)))
+        plot_network_comparison(base_network_pypsa_earth, eia_base_network, selected_voltage_class, "PyPSA-Earth base network", fig_name_map)
+        fig_name_intersection = pathlib.Path(plot_dir_path, "network_comparison_intersection_{}.png".format(str(selected_voltage_class)))
+        plot_intersection(base_network_pypsa_earth, eia_base_network, selected_voltage_class, fig_name_intersection)
 
 # Comparison for the number of crossings (PyPSA-Earth vs EIA)
 
@@ -130,10 +173,7 @@ for selected_voltage_class in eia_voltage_classes:
 # --> plot the EIA reference network and the PyPSA-Earth network
 
 eia_base_network_subset = eia_base_network[["ID", "TYPE", "VOLTAGE", "VOLT_CLASS", "SUB_1", "SUB_2", "SHAPE__Len", "geometry"]]
-eia_base_network_subset["geometry"] = eia_base_network_subset["geometry"].boundaries
-spatial_join_gadm_eia = eia_base_network_subset.sjoin(gadm_shapes_path, how="inner")[["ID", "TYPE", "GID_1", "VOLTAGE", "VOLT_CLASS", "SUB_1", "SUB_2", "SHAPE__Len", "ISO_1", "NAME_1", "geometry"]]
-
-
-
-
-# --> plot the intersections between the networks
+eia_base_network_subset["geometry"] = eia_base_network_subset["geometry"].boundary
+spatial_join_gadm_eia = eia_base_network_subset.sjoin(gadm_shapes, how="inner")[["ID", "TYPE", "GID_1", "VOLTAGE", "VOLT_CLASS", "SUB_1", "SUB_2", "SHAPE__Len", "ISO_1", "NAME_1", "geometry"]]
+count_df = (spatial_join_gadm_eia.groupby(["ID"])["ID"].count()-1).to_frame(name="crossings_count").reset_index()
+spatial_join_gadm_eia = pd.merge(spatial_join_gadm_eia, count_df, how="inner", on="ID")
