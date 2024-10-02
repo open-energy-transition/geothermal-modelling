c5f0c796a13a822a69d0067977a227647f5670a8
Fabrizio Finozzi <fabrizio.finozzi.business@gmail.com>
2024-10-02 18:57:36 +0200

    code: re-format  generation_comparison.py

--- analysis/scripts/generation_comparison.py
+++ analysis/scripts/generation_comparison.py
@@ -22,7 +22,6 @@ log_file_dir_path = pathlib.Path(base_path, "logs")
 plot_dir_path = pathlib.Path(base_path, "analysis", "plots")
 pypsa_earth_path = pathlib.Path(base_path, "workflow", "pypsa-earth")
 network_pypsa_earth_path = pathlib.Path(pypsa_earth_path, "results", "US_2021", "networks", "elec_s_10_ec_lcopt_Co2L-25H.nc")
-#network_pypsa_earth_path = pathlib.Path(pypsa_earth_path, "results", "US_2021", "networks", "elec_s_50_ec_lcopt_Co2L-24H.nc") # just to test it
 eia_generation_reference_path = pathlib.Path(base_path.parent, "geothermal-modelling", "analysis", "data", "EIA_statewise_data", "use_all_phy.xlsx")
 
 # Load data
@@ -38,13 +37,13 @@ args = parser.parse_args()
 # EIA select relevant year and relevant rows
 keywords_list = ["WYTCP", "NUETP", "HYTCP", "GEEGP"]
 eia_generation_reference_filtered = eia_generation_reference[eia_generation_reference["MSN"].isin(keywords_list)][["State", "MSN", args.year]]
-eia_generation_reference_filtered = eia_generation_reference_filtered.rename(columns={'MSN':'carrier'})
-eia_generation_reference_filtered.carrier = eia_generation_reference_filtered.carrier.map({"WYTCP":"onwind",
-                                                                                    "NUETP":"nuclear",
-                                                                                    "HYTCP":"hydro",
-                                                                                    "GEEGP":"geothermal"})
-eia_generation_reference_filtered.set_index(['carrier','State'],inplace=True)
-eia_generation_reference_filtered[2021] /= 1e3 #conversion from million kWh i.e., GWh to TWh
+eia_generation_reference_filtered = eia_generation_reference_filtered.rename(columns={'MSN': 'carrier'})
+eia_generation_reference_filtered.carrier = eia_generation_reference_filtered.carrier.map({"WYTCP": "onwind",
+                                                                                    "NUETP": "nuclear",
+                                                                                    "HYTCP": "hydro",
+                                                                                    "GEEGP": "geothermal"})
+eia_generation_reference_filtered.set_index(['carrier', 'State'] ,inplace=True)
+eia_generation_reference_filtered[args.year] /= 1e3 #conversion from million kWh i.e., GWh to TWh
 
 # Calculating the annual generation from each of the generators in MWh
 time_res = 24
@@ -58,23 +57,23 @@ network_pypsa_earth.generators.bus = network_pypsa_earth.generators.bus.str.repl
 network_pypsa_earth.generators.bus = network_pypsa_earth.generators.bus.map(usa_state_dict)
 
 # Selecting relevant data from PyPSA results
-pypsa_generation_filtered = network_pypsa_earth.generators.reset_index()[['carrier','bus','gen_MWh']]
-pypsa_generation_filtered = pypsa_generation_filtered.rename(columns={'bus':'State'})
-pypsa_generation_filtered.set_index(['carrier','State'],inplace=True)
+pypsa_generation_filtered = network_pypsa_earth.generators.reset_index()[['carrier', 'bus', 'gen_MWh']]
+pypsa_generation_filtered = pypsa_generation_filtered.rename(columns={'bus': 'State'})
+pypsa_generation_filtered.set_index(['carrier', 'State'], inplace=True)
 pypsa_generation_filtered['gen_MWh'] /= 1e6
 
 # Joining EIA and PyPSA generations to compare them
 comparison_generation = pypsa_generation_filtered.copy()
 comparison_generation = comparison_generation.join(eia_generation_reference_filtered)
-comparison_generation = comparison_generation.rename(columns={'gen_MWh':'PyPSA',2021:'EIA'})
+comparison_generation = comparison_generation.rename(columns={'gen_MWh': pypsa_name, args.year: eia_name})
 
 # iterate through each carrier, filter and plot the comparison between PyPSA and EIA generation statewise
-for car in ['nuclear','onwind','hydro','geothermal']:
+for car in ['nuclear', 'onwind', 'hydro', 'geothermal']:
     comparison_generation_filter = comparison_generation.reset_index().query('carrier == @car')
-    comparison_generation_filter = comparison_generation_filter.set_index('State')[['PyPSA','EIA']]
+    comparison_generation_filter = comparison_generation_filter.set_index('State')[[pypsa_name, eia_name]]
     fig = px.bar(comparison_generation_filter,barmode='group')
     fig.update_layout(title=f"Carrier = {car}",yaxis_title='Generation (TWh)')
     fig.show()
 
 # Compute error %s of the generation
-comparison_generation['error %'] = comparison_generation.apply(lambda x: (x['EIA']-x['PyPSA'])*100/x['EIA'], axis=1)
+comparison_generation['error %'] = comparison_generation.apply(lambda x: (x[eia_name]-x[pypsa_name])*100/x[eia_name], axis=1)
