e7fa5da34509760a4e39c6905907597f37c5e5fb
Fabrizio Finozzi <fabrizio.finozzi.business@gmail.com>
2024-10-10 11:41:07 +0200

    code: changes to the code

--- analysis/scripts/network_comparison.py
+++ analysis/scripts/network_comparison.py
@@ -142,20 +142,6 @@ def plot_network_crossings(pypsa_df, eia_df, color_dictionary, voltage_classes_l
     fig.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
     fig.write_image(pathlib.Path(plot_base_path, "gadm_state_lines_counts_by_voltage.png"))
 
-    state_lines_counts["delta_PyPSA"] = (state_lines_counts["PyPSA"] - state_lines_counts["EIA"]) / state_lines_counts["EIA"]*100.0
-    state_lines_counts["delta_PyPSA_parallel"] = (state_lines_counts["PyPSA_parallel"] - state_lines_counts["EIA"]) / state_lines_counts["EIA"]*100.0
-    state_lines_counts.to_csv(pathlib.Path(output_base_path, "gadm_state_lines_counts.csv"))
-
-    for voltage_class in voltage_classes_list:
-        ax1 = state_lines_counts.loc[state_lines_counts["v_nom_class"] == voltage_class].plot.scatter(x="state_0", y="delta_PyPSA", c="blue", label="PyPSA")
-        state_lines_counts.loc[state_lines_counts["v_nom_class"] == voltage_class].plot.scatter(x="state_0", y="delta_PyPSA_parallel", c="green", ax=ax1, label="PyPSA_parallel")
-        plt.xlabel("US state")
-        plt.ylabel("Error (%)")
-        plt.xticks(rotation="vertical")
-        plt.title("Voltage class: {}".format(voltage_class))
-        plt.subplots_adjust(bottom=0.3)
-        plt.savefig(pathlib.Path(plot_base_path, "gadm_state_lines_counts_for_voltage_{}.png".format(voltage_class)), dpi=800)
-
     ##############
     # IPM shapes #
     ##############
@@ -278,7 +264,6 @@ def parse_input_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("--plot_network_topology", help="Boolean: plot the network topology", action="store_true")
     parser.add_argument("--plot_network_crossings", help="Boolean: plot the network crossings", action="store_true")
-    parser.add_argument("--check_network_capacity", help="Boolean: check the network capacity", action="store_true")
     parser.add_argument("--plot_network_capacity", help="Boolean: plot the network capacity", action="store_true")
     return parser.parse_args()
 
@@ -330,6 +315,8 @@ def parse_inputs(base_path, log_file_dir_path):
     # joins with:
     #  -) the GADM shapes (level 1) to get the US state
     #  -) the IPM shapes to get the IPM region
+
+    # Bus 0
     eia_base_network_modified = eia_base_network.loc[:, ("OBJECTID_1", "sub_0_coors")]
     eia_base_network_modified["geometry"] = eia_base_network_modified["sub_0_coors"]
     log_output_file.write(" --> shape of eia_base_network before sub_0 spatial join {} \n".format(eia_base_network.shape))
@@ -340,6 +327,7 @@ def parse_inputs(base_path, log_file_dir_path):
     log_output_file.write(" --> shape of eia_base_network after sub_0 spatial join with ipm{} \n".format(spatial_join_ipm_eia_sub_0.shape))
     log_output_file.write(" --> shape of eia_base_network after sub_0 spatial join {} \n".format(spatial_join_eia_sub_0.shape))
 
+    # Bus 1
     eia_base_network_modified = eia_base_network.loc[:, ("OBJECTID_1", "sub_1_coors")]
     eia_base_network_modified["geometry"] = eia_base_network_modified["sub_1_coors"]
     log_output_file.write(" --> shape of eia_base_network before sub_1 spatial join {} \n".format(eia_base_network_modified.shape))
@@ -384,8 +372,8 @@ def parse_inputs(base_path, log_file_dir_path):
     #  -) the IPM shapes to get the IPM region
     log_output_file.write(" --> shape of pypsa-earth base network after reading it in {} \n".format(base_network_pypsa_earth.lines.shape))
 
+    # Bus 0
     base_network_pypsa_earth_geopandas_bus_0 = gpd.GeoDataFrame(base_network_pypsa_earth.lines, geometry=gpd.GeoSeries.from_wkt(base_network_pypsa_earth.lines.bus_0_coors), crs="EPSG:4326").reset_index()
-
     spatial_join_gadm_pearth_bus_0 = base_network_pypsa_earth_geopandas_bus_0.sjoin(gadm_shapes, how="left").loc[:, ("Line", "ISO_1")].rename(columns={"ISO_1": "state_0"})
     spatial_join_ipm_pearth_bus_0 = base_network_pypsa_earth_geopandas_bus_0.sjoin(ipm_shapes, how="left").loc[:, ("Line", "IPM_Region")].rename(columns={"IPM_Region": "ipm_region_0"})
     spatial_join_pearth_bus_0 = pd.merge(spatial_join_gadm_pearth_bus_0, spatial_join_ipm_pearth_bus_0, how="inner", on="Line")
@@ -393,8 +381,8 @@ def parse_inputs(base_path, log_file_dir_path):
     log_output_file.write(" --> shape of base.nc after sub_0 spatial join with ipm{} \n".format(spatial_join_ipm_pearth_bus_0.shape))
     log_output_file.write(" --> shape of base.nc after sub_0 spatial join {} \n".format(spatial_join_pearth_bus_0.shape))
 
+    # Bus 1
     base_network_pypsa_earth_geopandas_bus_1 = gpd.GeoDataFrame(base_network_pypsa_earth.lines, geometry=gpd.GeoSeries.from_wkt(base_network_pypsa_earth.lines.bus_1_coors), crs="EPSG:4326").reset_index()
-
     spatial_join_gadm_pearth_bus_1 = base_network_pypsa_earth_geopandas_bus_1.sjoin(gadm_shapes, how="left").loc[:, ("Line", "ISO_1")].rename(columns={"ISO_1": "state_1"})
     spatial_join_ipm_pearth_bus_1 = base_network_pypsa_earth_geopandas_bus_1.sjoin(ipm_shapes, how="left").loc[:, ("Line", "IPM_Region")].rename(columns={"IPM_Region": "ipm_region_1"})
     spatial_join_pearth_bus_1 = pd.merge(spatial_join_gadm_pearth_bus_1, spatial_join_ipm_pearth_bus_1, how="inner", on="Line")
@@ -450,9 +438,6 @@ if __name__ == '__main__':
 
     args = parse_input_arguments()
 
-    # Comparison for the network topologies (PyPSA-Earth vs EIA)
-    # --> plot the EIA reference network and the PyPSA-Earth network
-    # --> plot the intersections between the networks
     eia_voltage_classes = list(network_eia_df["v_nom_class"].unique())
 
     if args.plot_network_topology:
@@ -466,9 +451,12 @@ if __name__ == '__main__':
             plot_network_intersection(network_pypsa_df, network_eia_df, selected_voltage_class,
                                       fig_name_intersection)
 
-    # Comparison for the transmission crossings (PyPSA-Earth vs EIA)
+    # Comparison for the transmission crossings (PyPSA-Earth vs EIA) using:
+    # -) the GADM shapes(level 1) for the US state comparison
+    # -) the IPM shapes for the IPM region comparison
     if args.plot_network_crossings:
         plot_network_crossings(network_pypsa_df, network_eia_df, ccs_color_dict, eia_voltage_classes, output_path, plot_path)
 
+    # Comparison for the transmission capacities (PyPSA-Earth vs IPM transmission capacities)
     if args.plot_network_capacity:
         plot_network_capacity(network_pypsa_df, ccs_color_dict, default_path, output_path, plot_path)
