f73623305f239008782ace68d525b492fcff0a5f
Sermisha <sermisha.narayana@openenergytransition.org>
2025-02-21 16:08:00 +0530

    code: Load aggregate plot across sectors

--- analysis/scripts/plot_energy_balance.py
+++ analysis/scripts/plot_energy_balance.py
@@ -69,51 +69,60 @@ def get_generations(pypsa_network, sector_array):
         energy_generations = energy_generations.join(sector_generations, how='left',lsuffix='_x', rsuffix='_y')
     return energy_generations
 
+def get_loads(pypsa_network, sector_array):
+    loads = pd.DataFrame(index=pypsa_network.snapshots)
+    for sector in sector_array:
+        sector_buses = pypsa_network.buses.index[pypsa_network.buses.carrier.str.contains(sector)]
+        load_sector_columns = pypsa_network.loads.query('bus in @sec', local_dict={'sec':sector_buses}).index
+        load_sector_ts = pypsa_network.loads_t.p[load_sector_columns].groupby(pypsa_network.loads.carrier, axis=1).sum().div(1e3)
 
-def plot_electricity_balance(pypsa_network, renewable_carriers, conventional_carriers, storage_unit_carriers):
-    # ac_buses = pypsa_network.buses.query("carrier == 'AC'")
-    links = (
-        pypsa_network.links_t.p1.groupby(pypsa_network.links.carrier, axis=1).sum().div(1e3) * -1
-    )
-    conventional_energy = links[link_carriers]
-    p_by_carrier = conventional_energy
+        loads = loads.join(load_sector_ts, how='left', lsuffix='_x', rsuffix='_y')
+    return loads
 
-    generators = pypsa_network.generators_t.p.groupby(pypsa_network.generators.carrier, axis=1).sum().div(1e3)
-    renewable_energy = generators[generator_carriers]
-    p_by_carrier = pd.concat([p_by_carrier,generators],axis=1)
+# def plot_electricity_balance(pypsa_network, renewable_carriers, conventional_carriers, storage_unit_carriers):
+#     # ac_buses = pypsa_network.buses.query("carrier == 'AC'")
+#     links = (
+#         pypsa_network.links_t.p1.groupby(pypsa_network.links.carrier, axis=1).sum().div(1e3) * -1
+#     )
+#     conventional_energy = links[link_carriers]
+#     p_by_carrier = conventional_energy
 
-    if not pypsa_network.storage_units.empty:
-        sto = (
-            pypsa_network.storage_units_t.p.groupby(pypsa_network.storage_units.carrier, axis=1).sum().div(1e3)
-        )
-        p_by_carrier = pd.concat([p_by_carrier,sto],axis=1)
+#     generators = pypsa_network.generators_t.p.groupby(pypsa_network.generators.carrier, axis=1).sum().div(1e3)
+#     renewable_energy = generators[generator_carriers]
+#     p_by_carrier = pd.concat([p_by_carrier,generators],axis=1)
 
-        fig, ax = plt.subplots(figsize=(6, 3))
+#     if not pypsa_network.storage_units.empty:
+#         sto = (
+#             pypsa_network.storage_units_t.p.groupby(pypsa_network.storage_units.carrier, axis=1).sum().div(1e3)
+#         )
+#         p_by_carrier = pd.concat([p_by_carrier,sto],axis=1)
 
-    # color = p_by_carrier.columns.map(pypsa_network.carriers.color)
+#         fig, ax = plt.subplots(figsize=(6, 3))
+
+#     # color = p_by_carrier.columns.map(pypsa_network.carriers.color)
     
-    p_by_carrier.where(p_by_carrier > 0).plot.area(
-        ax=ax,
-        linewidth=0,
-        # color=color,
-    )
+#     p_by_carrier.where(p_by_carrier > 0).plot.area(
+#         ax=ax,
+#         linewidth=0,
+#         # color=color,
+#     )
 
-    charge = p_by_carrier.where(p_by_carrier < 0).dropna(how="all", axis=1)
+#     charge = p_by_carrier.where(p_by_carrier < 0).dropna(how="all", axis=1)
 
-    if not charge.empty:
-        charge.plot.area(
-            ax=ax,
-            linewidth=0,
-            # color=charge.columns.map(pypsa_network.carriers.color)
-        )
+#     if not charge.empty:
+#         charge.plot.area(
+#             ax=ax,
+#             linewidth=0,
+#             # color=charge.columns.map(pypsa_network.carriers.color)
+#         )
 
-    # (pypsa_network.loads_t.p_set.sum(axis=1).loc[time].div(1e3)).plot(ax=ax, c="k",linewidth = 0.1)
-    plt.legend(loc=(1.05, 0),fontsize=5,reverse=True)
-    ax.set_ylabel("GW")
-    # ax.set_ylim(-5, 30)
-    plt.subplots_adjust(right=0.8)
-    ax.grid(linestyle="--")
-    return p_by_carrier
+#     # (pypsa_network.loads_t.p_set.sum(axis=1).loc[time].div(1e3)).plot(ax=ax, c="k",linewidth = 0.1)
+#     plt.legend(loc=(1.05, 0),fontsize=5,reverse=True)
+#     ax.set_ylabel("GW")
+#     # ax.set_ylim(-5, 30)
+#     plt.subplots_adjust(right=0.8)
+#     ax.grid(linestyle="--")
+#     return p_by_carrier
 
 def installed_capacity_plots(pypsa_network, plot_base_path):
     installed_capacities = get_capacities(pypsa_network,['AC','heat','water','H2','oil','gas','biomass'])
@@ -134,12 +143,17 @@ def energy_generation_plots(pypsa_network, plot_base_path):
         f"{plot_base_path}/energy_generation_sectorwise_TWh.png", scale=1.5
     )
 
+def load_plots(pypsa_network, plot_base_path):
+    loads = get_loads(pypsa_network,['low voltage','heat','water','H2','oil','gas','biomass'])
+    time_granularity = pypsa_network.snapshots.diff()[-1].total_seconds() / 3600
+    loads_agg = loads.sum() * time_granularity / 1e3 #in TWh
+    loads_agg.name = 'load in TWh'
+
+    fig = px.bar(loads_agg, y='load in TWh', barmode='group', height=800)
+    fig.write_image(
+        f"{plot_base_path}/loads_sectorwise_TWh.png", scale=1.5
+    )
 
 pypsa_network_path = pathlib.Path("workflow","pypsa-earth","results","postnetworks","elec_s_100_ec_lcopt_Co2L-24H_144H_2030_0.071_AB_10export.nc")
 pypsa_network = pypsa.Network(pypsa_network_path)
-
-renewable_carriers = config['electricity']['renewable_carriers']
-conventional_carriers = config['electricity']['conventional_carriers']
-storage_unit_carriers = ['PHS', 'hydro']
-
-p_by_carrier = plot_electricity_balance(pypsa_network, renewable_carriers, conventional_carriers, storage_unit_carriers)
+plot_base_path = pathlib.Path("analysis", "outputs", "summary")
