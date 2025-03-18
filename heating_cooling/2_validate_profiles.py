f5f0e17561708f21e7f42615f8702b80be8f8731
Ekaterina <e.v.kasilova@gmail.com>
2025-03-18 23:44:20 +0100

    [pre-commit.ci] auto fixes from pre-commit.com hooks (#58)
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- heating_cooling/2_validate_profiles.py
+++ heating_cooling/2_validate_profiles.py
@@ -13,24 +13,66 @@
 
 import atlite
 import os
-import pypsa
 
-import geopandas as gpd
-import matplotlib.pyplot as plt
-import xarray as xr
 
 import datetime as dt
 import numpy as np
 import pandas as pd
 
-STATES = ['AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'IA',
-       'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN',
-       'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY',
-       'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA',
-       'VT', 'WA', 'WI', 'WV', 'WY']
+STATES = [
+    "AL",
+    "AR",
+    "AZ",
+    "CA",
+    "CO",
+    "CT",
+    "DC",
+    "DE",
+    "FL",
+    "GA",
+    "IA",
+    "ID",
+    "IL",
+    "IN",
+    "KS",
+    "KY",
+    "LA",
+    "MA",
+    "MD",
+    "ME",
+    "MI",
+    "MN",
+    "MO",
+    "MS",
+    "MT",
+    "NC",
+    "ND",
+    "NE",
+    "NH",
+    "NJ",
+    "NM",
+    "NV",
+    "NY",
+    "OH",
+    "OK",
+    "OR",
+    "PA",
+    "RI",
+    "SC",
+    "SD",
+    "TN",
+    "TX",
+    "UT",
+    "VA",
+    "VT",
+    "WA",
+    "WI",
+    "WV",
+    "WY",
+]
 
-output_prefix = "commercial"   # "residential" or "commercial"
-load_type = "cooling" # "cooling" or "heating"
+output_prefix = "commercial"  # "residential" or "commercial"
+load_type = "cooling"  # "cooling" or "heating"
 
 t_threshold = 22
 
@@ -41,9 +83,10 @@ cutout_path = "./pypsa-earth/cutouts"
 cutout_fl = "us-2018-era5.nc"
 
 ref_data_dir = "./_heating implementation_/_data processing_/_res_/timeseries"
-ref_data_fl = output_prefix + "_nrel_" + load_type + "_2018_ref.csv" 
+ref_data_fl = output_prefix + "_nrel_" + load_type + "_2018_ref.csv"
 
-res_dir = "./_heating implementation_/_data processing_/_res_" 
+res_dir = "./_heating implementation_/_data processing_/_res_"
+
 
 # calculation of climate parameters -------------------------------------------
 def RH(t, td):
@@ -58,14 +101,16 @@ def RH(t, td):
     f = 100 * np.exp(a)
     return f
 
+
 # https://www.conservationphysics.org/atmcalc/atmoclc1.html
 def p_s(t):
     """
     p_s is the water vapor saturation pressure [Pa]
     t is ambient air temperature [C]
     """
-    p_s = 610.78 * np.exp( t / ( t + 238.3 ) * 17.2694 )
+    p_s = 610.78 * np.exp(t / (t + 238.3) * 17.2694)
     return p_s
+
 
 # kg water vapour / kg dry air = 0.62 *10-5 *p
 def AH(t, RH):
@@ -75,7 +120,8 @@ def AH(t, RH):
     """
     p = RH * p_s(t)
     ah = p * 0.62e-5
-    return ah        
+    return ah
+
 
 # pure PyPSA-Eur approach -----------------------------------------------------
 def convert_heat_demand1(ds, threshold, a, constant, hour_shift):
@@ -84,7 +130,7 @@ def convert_heat_demand1(ds, threshold, a, constant, hour_shift):
     T = T.assign_coords(
         time=(T.coords["time"] + np.timedelta64(dt.timedelta(hours=hour_shift)))
     )
-    T = T.resample(time="1D").mean(dim="time") 
+    T = T.resample(time="1D").mean(dim="time")
 
     threshold += 273.15
     heat_demand = a * (threshold - T)
@@ -102,7 +148,8 @@ def heat_demand1(cutout, threshold=15.0, a=1.0, constant=0.0, hour_shift=0.0, **
         constant=constant,
         hour_shift=hour_shift,
         **params,
-    )     
+    )
+
 
 def convert_cooling_demand1(ds, threshold, a, constant, hour_shift):
     # Temperature is in Kelvin; take daily average
@@ -110,7 +157,7 @@ def convert_cooling_demand1(ds, threshold, a, constant, hour_shift):
     T = T.assign_coords(
         time=(T.coords["time"] + np.timedelta64(dt.timedelta(hours=hour_shift)))
     )
-    T = T.resample(time="1D").mean(dim="time") 
+    T = T.resample(time="1D").mean(dim="time")
 
     threshold += 273.15
     cooling_demand = a * (T - threshold)
@@ -120,7 +167,9 @@ def convert_cooling_demand1(ds, threshold, a, constant, hour_shift):
     return (constant + cooling_demand).rename("cooling_demand")
 
 
-def cooling_demand1(cutout, threshold=23.0, a=1.0, constant=0.0, hour_shift=0.0, **params):
+def cooling_demand1(
+    cutout, threshold=23.0, a=1.0, constant=0.0, hour_shift=0.0, **params
+):
     return cutout.convert_and_aggregate(
         convert_func=convert_cooling_demand1,
         threshold=threshold,
@@ -129,6 +178,7 @@ def cooling_demand1(cutout, threshold=23.0, a=1.0, constant=0.0, hour_shift=0.0,
         hour_shift=hour_shift,
         **params,
     )
+
 
 # adding meteo-corrections styled according to demand-ninja -------------------
 def convert_heat_demand2(ds, threshold, a, constant, hour_shift):
@@ -139,12 +189,14 @@ def convert_heat_demand2(ds, threshold, a, constant, hour_shift):
     )
     T = T.resample(time="1D").mean(dim="time")
 
-    #(0.012 * cutout_atl.data["influx_toa"][1]).plot()
+    # (0.012 * cutout_atl.data["influx_toa"][1]).plot()
     dT_radiation = 0.012 * ds["influx_toa"]
     dT_radiation = dT_radiation.assign_coords(
-        time=(dT_radiation.coords["time"] + np.timedelta64(dt.timedelta(hours=hour_shift)))
+        time=(
+            dT_radiation.coords["time"] + np.timedelta64(dt.timedelta(hours=hour_shift))
+        )
     )
-    dT_radiation = dT_radiation.resample(time="1D").mean(dim="time")    
+    dT_radiation = dT_radiation.resample(time="1D").mean(dim="time")
 
     threshold += 273.15
     heat_demand = a * (threshold - T - dT_radiation)
@@ -162,7 +214,8 @@ def heat_demand2(cutout, threshold=15.0, a=1.0, constant=0.0, hour_shift=0.0, **
         constant=constant,
         hour_shift=hour_shift,
         **params,
-    )     
+    )
+
 
 def convert_heat_demand3(ds, threshold, a, constant, hour_shift):
     # Temperature is in Kelvin; take daily average
@@ -172,10 +225,12 @@ def convert_heat_demand3(ds, threshold, a, constant, hour_shift):
     )
     T = T.resample(time="1D").mean(dim="time")
 
-    #(0.012 * cutout_atl.data["influx_toa"][1]).plot()
+    # (0.012 * cutout_atl.data["influx_toa"][1]).plot()
     dT_radiation = 0.012 * ds["influx_toa"]
     dT_radiation = dT_radiation.assign_coords(
-        time=(dT_radiation.coords["time"] + np.timedelta64(dt.timedelta(hours=hour_shift)))
+        time=(
+            dT_radiation.coords["time"] + np.timedelta64(dt.timedelta(hours=hour_shift))
+        )
     )
     dT_radiation = dT_radiation.resample(time="1D").mean(dim="time")
 
@@ -185,7 +240,7 @@ def convert_heat_demand3(ds, threshold, a, constant, hour_shift):
     T_smooth = T_apparent.rolling(time=5, min_periods=2, center=True).mean()
 
     ## numbagg >= 0.2.1 is required for rolling_exp but currently version {_NUMBAGG_VERSION} is installed
-    #T_smooth = T.rolling_exp(time=3).mean()
+    # T_smooth = T.rolling_exp(time=3).mean()
 
     threshold += 273.15
     heat_demand = a * (threshold - T_smooth)
@@ -205,6 +260,7 @@ def heat_demand3(cutout, threshold=15.0, a=1.0, constant=0.0, hour_shift=0.0, **
         **params,
     )
 
+
 def convert_heat_demand5(ds, threshold, a, constant, hour_shift):
     # Temperature is in Kelvin; take daily average
     T = ds["temperature"]
@@ -216,7 +272,9 @@ def convert_heat_demand5(ds, threshold, a, constant, hour_shift):
     # 0.012 ± 0.008 °C W−1 m−2
     dT_radiation = 0.012 * ds["influx_toa"]
     dT_radiation = dT_radiation.assign_coords(
-        time=(dT_radiation.coords["time"] + np.timedelta64(dt.timedelta(hours=hour_shift)))
+        time=(
+            dT_radiation.coords["time"] + np.timedelta64(dt.timedelta(hours=hour_shift))
+        )
     )
     dT_radiation = dT_radiation.resample(time="1D").mean(dim="time")
 
@@ -224,14 +282,14 @@ def convert_heat_demand5(ds, threshold, a, constant, hour_shift):
     phi = RH((T - 273.15), (Td - 273.15))
 
     # 0.050 ± 0.065 °C (g kg−1)−1
-    dT_apparent_phi = 50 * AH((T - 273.15), phi/100)
+    dT_apparent_phi = 50 * AH((T - 273.15), phi / 100)
 
     T_apparent = T + dT_radiation + dT_apparent_phi
 
     T_smooth = T_apparent.rolling(time=3, min_periods=2, center=True).mean()
 
     ## numbagg >= 0.2.1 is required for rolling_exp but currently version {_NUMBAGG_VERSION} is installed
-    #T_smooth = T.rolling_exp(time=3).mean()
+    # T_smooth = T.rolling_exp(time=3).mean()
 
     threshold += 273.15
     heat_demand = a * (threshold - T_smooth)
@@ -251,18 +309,21 @@ def heat_demand5(cutout, threshold=15.0, a=1.0, constant=0.0, hour_shift=0.0, **
         **params,
     )
 
+
 def convert_cooling_demand5(ds, threshold, a, constant, hour_shift):
     # Temperature is in Kelvin; take daily average
     T = ds["temperature"]
     T = T.assign_coords(
         time=(T.coords["time"] + np.timedelta64(dt.timedelta(hours=hour_shift)))
     )
-    T = T.resample(time="1D").mean(dim="time") 
+    T = T.resample(time="1D").mean(dim="time")
 
     # 0.012 ± 0.008 °C W−1 m−2
     dT_radiation = 0.012 * ds["influx_toa"]
     dT_radiation = dT_radiation.assign_coords(
-        time=(dT_radiation.coords["time"] + np.timedelta64(dt.timedelta(hours=hour_shift)))
+        time=(
+            dT_radiation.coords["time"] + np.timedelta64(dt.timedelta(hours=hour_shift))
+        )
     )
     dT_radiation = dT_radiation.resample(time="1D").mean(dim="time")
 
@@ -270,15 +331,14 @@ def convert_cooling_demand5(ds, threshold, a, constant, hour_shift):
     phi = RH((T - 273.15), (Td - 273.15))
 
     # 0.050 ± 0.065 °C (g kg−1)−1
-    dT_apparent_phi = 50 * AH((T - 273.15), phi/100)
+    dT_apparent_phi = 50 * AH((T - 273.15), phi / 100)
 
     T_apparent = T + dT_radiation + dT_apparent_phi
 
     T_smooth = T_apparent.rolling(time=3, min_periods=2, center=True).mean()
 
     ## numbagg >= 0.2.1 is required for rolling_exp but currently version {_NUMBAGG_VERSION} is installed
-    #T_smooth = T.rolling_exp(time=3).mean()
-
+    # T_smooth = T.rolling_exp(time=3).mean()
 
     threshold += 273.15
     cooling_demand = a * (T_smooth - threshold)
@@ -288,7 +348,9 @@ def convert_cooling_demand5(ds, threshold, a, constant, hour_shift):
     return (constant + cooling_demand).rename("cooling_demand")
 
 
-def cooling_demand5(cutout, threshold=23.0, a=1.0, constant=0.0, hour_shift=0.0, **params):
+def cooling_demand5(
+    cutout, threshold=23.0, a=1.0, constant=0.0, hour_shift=0.0, **params
+):
     return cutout.convert_and_aggregate(
         convert_func=convert_cooling_demand1,
         threshold=threshold,
@@ -299,10 +361,17 @@ def cooling_demand5(cutout, threshold=23.0, a=1.0, constant=0.0, hour_shift=0.0,
     )
 
 
-
 # read inputs -----------------------------------------------------------------
 
-print("ts_" + output_prefix + "_nrel_" + load_type + "_t_th_" + str(t_threshold) + "_2018_ref.csv")
+print(
+    "ts_"
+    + output_prefix
+    + "_nrel_"
+    + load_type
+    + "_t_th_"
+    + str(t_threshold)
+    + "_2018_ref.csv"
+)
 
 # reference data NREL database
 ref_df = pd.read_csv(os.path.join(ref_data_dir, ref_data_fl))
@@ -324,27 +393,21 @@ else:
 # validation run --------------------------------------------------------------
 
 valid_ts_list = [None] * len(STATES)
-valid_corr_df = pd.DataFrame(
-    {
-        "state": STATES,
-        "corr_with_model": None
-    }
-)
+valid_corr_df = pd.DataFrame({"state": STATES, "corr_with_model": None})
 
 for i, st in enumerate(STATES):
     x_state = ref_df[ref_df.state == st]["x"].iloc[0]
     y_state = ref_df[ref_df.state == st]["y"].iloc[0]
 
-    mod_demand_ts_df = (
-        test1_ts
-        .sel(x=x_state, y=y_state, method='nearest')
-        .to_dataframe()
-    )
+    mod_demand_ts_df = test1_ts.sel(
+        x=x_state, y=y_state, method="nearest"
+    ).to_dataframe()
 
     ref_demand_ts = (
-        #ref_df[(pd.to_datetime(ref_df.time_date).dt.month == 1) & (ref_df.state == st) & (pd.to_datetime(ref_df.time_date).dt.year == 2018)]
-        ref_df[(ref_df.state == st) & (pd.to_datetime(ref_df.time_date).dt.year == 2018)]
-        .sort_values("time_date")[["time_date", "load_daily", "state"]]
+        # ref_df[(pd.to_datetime(ref_df.time_date).dt.month == 1) & (ref_df.state == st) & (pd.to_datetime(ref_df.time_date).dt.year == 2018)]
+        ref_df[
+            (ref_df.state == st) & (pd.to_datetime(ref_df.time_date).dt.year == 2018)
+        ].sort_values("time_date")[["time_date", "load_daily", "state"]]
     )
 
     ref_demand_ts.time_date = ref_demand_ts.time_date + " 00:00:00"
@@ -354,7 +417,6 @@ for i, st in enumerate(STATES):
     valid_df = mod_demand_ts_df.join(ref_demand)
     valid_ts_list[i] = valid_df
 
-    
     if load_type == "cooling":
         corr_val = valid_df["cooling_demand"].corr(valid_df["load_daily"])
         mean_mod_dem = mod_demand_ts_df["cooling_demand"].mean()
@@ -379,7 +441,12 @@ for i, st in enumerate(STATES):
 valid_corr_df.to_csv(
     os.path.join(
         res_dir,
-        output_prefix + "_nrel_" + load_type + "_t_th_" + str(t_threshold) + "_2018_baseline.csv" 
+        output_prefix
+        + "_nrel_"
+        + load_type
+        + "_t_th_"
+        + str(t_threshold)
+        + "_2018_baseline.csv",
     )
 )
 
@@ -387,8 +454,12 @@ valid_ts_df = pd.concat(valid_ts_list)
 valid_ts_df.to_csv(
     os.path.join(
         res_dir,
-        "ts_" + output_prefix + "_nrel_" + load_type + "_t_th_" + str(t_threshold) + "_2018_baseline.csv" 
+        "ts_"
+        + output_prefix
+        + "_nrel_"
+        + load_type
+        + "_t_th_"
+        + str(t_threshold)
+        + "_2018_baseline.csv",
     )
-) 
-
-
+)
