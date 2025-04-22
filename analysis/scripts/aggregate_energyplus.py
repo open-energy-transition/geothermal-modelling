56256eaf0123bfe215a8457f91f0290184871512
ekatef <e.v.kasilova@gmail.com>
2025-04-23 00:24:06 +0200

    Rename energyplus script to match other scripts

---
+++ analysis/scripts/aggregate_energyplus.py
@@ -0,0 +1,216 @@
+import logging
+import pandas as pd
+import geopandas as gpd
+import os
+import pathlib
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[logging.FileHandler("energyplus_aggregate.log"), logging.StreamHandler()],
+)
+
+
+def get_state_id(
+    state_fl_name,
+    file_suff,
+    abbrev_df,
+    pumas_df,
+):
+    state_abbr = str(state_fl_name).replace(file_suff, "").upper()
+    state_full_name = abbrev_df[abbrev_df.state == state_abbr].full_name.to_list()[0]
+    state_id = pumas_df[pumas_df.State == state_full_name].STATEFIP.unique()[0]
+    state_geoid = str(state_id)
+
+    return state_geoid
+
+def consolidate_pumas(
+    data_path,
+    states_abbr_df,
+    puma_centroid_merged,
+):
+    # consolidating load profiles
+    data_state_fls = list(data_path.iterdir())
+
+    data_state_fls_clean = [fl_path for fl_path in data_state_fls if fl_path.name not in ["ak.csv", "hi.csv"]]    
+
+    # a single time-series dataframe is needed to look-up for each PUMA -----------
+    data_ts_national_list = [None] * len(data_state_fls_clean)
+
+    logger.info("Build a consolidated national-wide load dataframe")
+    for i, st_fl_path in enumerate(data_state_fls_clean):
+        logger.info("Consolidation for " + str(st_fl_path.name))
+        state_heat_df = pd.read_csv(
+            pathlib.Path(st_fl_path),
+        ).set_index("time")
+
+        # the column names should correspond to GEOID to make further lookup work
+        state_geoid = get_state_id(
+            st_fl_path.name,
+            file_suff=".csv",
+            abbrev_df=states_abbr_df,
+            pumas_df=puma_centroid_merged,
+        )
+        state_heat_df.columns = [state_geoid + col for col in state_heat_df.columns]
+        data_ts_national_list[i] = state_heat_df
+
+    data_ts_national_df = pd.concat(data_ts_national_list, axis=1)
+
+    return data_ts_national_df
+
+def lookup_bus_pumas(
+    data_ts_national_df,
+    bus,
+    bus_pumas
+):
+    # some PUMAs can be missed from the time-series data columns
+    pumas_in_data = data_ts_national_df.columns.intersection(
+        bus_pumas
+    ).to_list()
+    bus_pumas_df = pd.DataFrame(index=data_ts_national_df.index)
+    bus_pumas_df[bus] = data_ts_national_df[pumas_in_data].sum(
+        axis=1
+    )
+    return bus_pumas_df   
+
+
+if __name__ == "__main__":
+    if "snakemake" not in globals():
+        from _helpers_usa import mock_snakemake
+
+        snakemake = mock_snakemake("aggregate_energyplus")
+
+    default_path = pathlib.Path(__file__).parent.parent.parent
+
+    state_resstock_heat_dir = pathlib.Path(default_path, snakemake.input.state_resstock_heat_dir)
+    state_resstock_cool_dir = pathlib.Path(default_path, snakemake.input.state_resstock_cool_dir)
+
+    state_comstock_heat_dir = pathlib.Path(default_path, snakemake.input.state_comstock_heat_dir)
+    state_comstock_cool_dir = pathlib.Path(default_path, snakemake.input.state_comstock_cool_dir)
+
+
+    shapes_path = pathlib.Path(default_path, snakemake.input.shapes_path)
+    puma_path = pathlib.Path(default_path, snakemake.input.puma_path)
+    states_path = pathlib.Path(default_path, snakemake.input.states_path)
+
+    heat_demand_path = pathlib.Path(default_path, snakemake.output.heat_demand_path)
+    cool_demand_path = pathlib.Path(default_path, snakemake.output.cool_demand_path)
+
+    # gis information is processed for the model and PUMA regions
+    # to match demand time series for the model bus regions
+    model_gdf = gpd.read_file(shapes_path)
+    puma_gdf = gpd.read_file(puma_path).to_crs(model_gdf.crs)
+
+    # abbreviation vs full names transformaiton is needed
+    # to deal with transformaitons of PUMA codes
+    states_abbr_df = pd.read_csv(states_path)
+
+    puma_centroid = puma_gdf.copy()
+    puma_centroid.geometry = puma_centroid.geometry.centroid
+    puma_centroid_merged = gpd.sjoin_nearest(puma_centroid, model_gdf, how="left")
+
+    # consolidating load profiles
+    resstock_heating_ts_national_df = consolidate_pumas(
+        data_path=state_resstock_heat_dir,
+        states_abbr_df=states_abbr_df,
+        puma_centroid_merged=puma_centroid_merged,
+    )
+
+    resstock_cooling_ts_national_df = consolidate_pumas(
+        data_path=state_resstock_cool_dir,
+        states_abbr_df=states_abbr_df,
+        puma_centroid_merged=puma_centroid_merged,
+    )
+
+    comstock_heating_ts_national_df = consolidate_pumas(
+        data_path=state_comstock_heat_dir,
+        states_abbr_df=states_abbr_df,
+        puma_centroid_merged=puma_centroid_merged,
+    )
+
+    comstock_cooling_ts_national_df = consolidate_pumas(
+        data_path=state_comstock_cool_dir,
+        states_abbr_df=states_abbr_df,
+        puma_centroid_merged=puma_centroid_merged,
+    )
+
+    # time-series for each PUMA should be aggregated ------------------------------
+    load_buses = puma_centroid_merged.name.unique()
+
+    resstock_pumas_heating_list = [None] * len(load_buses)
+    comstock_pumas_heating_list = [None] * len(load_buses)
+    
+    pumas_cooling_list = [None] * len(load_buses)    
+
+    logger.info("Aggregate by PUMAs")
+    for i, bus in enumerate(load_buses):
+        logger.info("Load aggreagtion for a bus: " + bus)
+        bus_pumas = puma_centroid_merged[puma_centroid_merged.name == bus]["GEOID"]
+
+        # resstock ------------------------------------------------------------
+        resstock_pumas_heating_df = lookup_bus_pumas(
+            data_ts_national_df=resstock_heating_ts_national_df,
+            bus=bus,
+            bus_pumas=bus_pumas,
+        )
+        resstock_pumas_heating_list[i] = resstock_pumas_heating_df
+
+        resstock_pumas_cooling_df = lookup_bus_pumas(
+            data_ts_national_df=resstock_cooling_ts_national_df,
+            bus=bus,
+            bus_pumas=bus_pumas,
+        )
+
+        # comstock ------------------------------------------------------------
+        comstock_pumas_heating_df = lookup_bus_pumas(
+            data_ts_national_df=comstock_heating_ts_national_df,
+            bus=bus,
+            bus_pumas=bus_pumas,
+        )
+        comstock_pumas_heating_list[i] = comstock_pumas_heating_df
+
+        comstock_pumas_cooling_df = lookup_bus_pumas(
+            data_ts_national_df=comstock_cooling_ts_national_df,
+            bus=bus,
+            bus_pumas=bus_pumas,
+        )
+
+        # for cooling we don't distinguish between the residential and services sector
+        pumas_cooling_list[i] = resstock_pumas_cooling_df + comstock_pumas_cooling_df
+
+    resstock_heating_load_aggreg_df = pd.concat(resstock_pumas_heating_list, axis=1)
+    comstock_heating_load_aggreg_df = pd.concat(comstock_pumas_heating_list, axis=1)
+    
+    cooling_load_aggreg_df = pd.concat(pumas_cooling_list, axis=1)    
+
+    # Resstock
+    print("Heating overall -- Resstock")
+    print(resstock_heating_load_aggreg_df.sum().sum() / 1e6)
+    print("Heating: cross-checking by PUMAs df -- Resstock")
+    print(resstock_heating_ts_national_df.sum().sum() / 1e6)
+
+    # Comstock
+    print("Heating overall -- Comstock")
+    print(comstock_heating_load_aggreg_df.sum().sum() / 1e6)
+    print("Heating: cross-checking by PUMAs df -- Comstock")
+    print(comstock_heating_ts_national_df.sum().sum() / 1e6)
+
+    print("Cooling overall -- Comstock")
+    print(cooling_load_aggreg_df.sum().sum() / 1e6)
+
+    print("Cooling: cross-checking by PUMAs df -- Resstock + Comstock")
+    print(resstock_cooling_ts_national_df.sum().sum() / 1e6 + comstock_cooling_ts_national_df.sum().sum() / 1e6)
+
+    # A temporaly solution for warm water
+    resstock_water_df = pd.DataFrame(
+        index=resstock_heating_load_aggreg_df.index,
+        data=[0.20 * resstock_heating_load_aggreg_df.sum(axis=0)] * len(resstock_heating_load_aggreg_df.index),
+    )
+    comstock_water_df = pd.DataFrame(
+        index=comstock_heating_load_aggreg_df.index,
+        data=[0.20 * comstock_heating_load_aggreg_df.sum(axis=0)] * len(comstock_heating_load_aggreg_df.index),
+    )    
+
+    resstock_heating_load_aggreg_df.to_csv(heat_demand_path)
+    resstock_cooling_load_aggreg_df.to_csv(cool_demand_path)
