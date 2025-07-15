604e48bf32e71e8f8a8c4bf222e60754aeb1d207
Ekaterina <e.v.kasilova@gmail.com>
2025-07-15 21:09:20 +0200

    Add thermal growthrates (#80)
    * Add filtering by onshore PUMAS
    
    * Make filtering by islands optional
    
    * Add growth rates for thermal loads
    
    * Add growth-rates parameters
    
    * Add growth rates inputs
    
    * Add an extractor function for the thermal load growth rates
    
    * Update the naming
    
    * Add a function to evaluate the growth rates
    
    * Define a dictionary for thermal sectors
    
    * Implement growth-rates scaling
    
    * Update a comment
    
    * Add a growth-rates input in Snakemake
    
    * Fix a typo
    
    * Add pre-commit fixes
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * shift parameters from  to  in  rule
    
    * code: Snakefile path change for thermal growth rates
    
    * reading aggregate_energyplus parameters from config file
    
    * Add a missed config parameter
    
    * Amend a hint on the available scenarios options
    
    * Fix merge
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Narayana <snarayana@htc-login001.zib.de>
    Co-authored-by: Sermisha <sermisha.narayana@openenergytransition.org>

--- analysis/scripts/aggregate_energyplus.py
+++ analysis/scripts/aggregate_energyplus.py
@@ -14,7 +14,8 @@ logging.basicConfig(
 # TODO Put the parameters into the config
 SHARE_WATER_SH_DEMAND = 0.20
 RATIO_SERV_TO_RESID = 1
-SIMPLIFIED_WRMWATER = False
+SIMPLIFIED_WRMWATER = True
+FILTER_ISLAND_PUMAS = False
 DATA_IS_SCALED = (
     True  # post processing was done externally to workflow, presented as MWh
 )
@@ -27,6 +28,15 @@ RESID_COOLING_TOTAL = 0.25 * 3.3e9  # [MWh]
 
 SERVICE_HEATING_TOTAL = 0.25 * 3.3e9  # [MWh]
 SERVICE_COOLING_TOTAL = 0.25 * 3.3e9  # [MWh]
+
+THERM_LOAD_SECTORS = [
+    "residential heating",
+    "services heating",
+    "residential water",
+    "services water",
+    "residential cooling",
+    "services cooling",
+]
 
 
 def get_state_id(
@@ -98,6 +108,7 @@ def consolidate_pumas(
             abbrev_df=states_abbr_df,
             pumas_df=puma_centroid_merged,
         )
+
         state_heat_df.columns = [state_geoid + col for col in state_heat_df.columns]
         data_ts_national_list[i] = state_heat_df
 
@@ -105,6 +116,25 @@ def consolidate_pumas(
     data_ts_national_df.fillna(0, inplace=True)
 
     return data_ts_national_df
+
+
+def find_onshore_pumas(puma_centroid_merged):
+    if "distances" in puma_centroid_merged.columns:
+        i_pumas_inside_onshore = puma_centroid_merged.distances == 0
+        codes_pumas_inside_onshore = (
+            puma_centroid_merged[i_pumas_inside_onshore].STATEFIP
+            + puma_centroid_merged[i_pumas_inside_onshore].PUMA
+        )
+    return codes_pumas_inside_onshore
+
+
+def filter_by_island_pumas(puma_centroid_merged, data_df):
+    onshore_codes = find_onshore_pumas(puma_centroid_merged)
+    onshore_data_cols = resstock_heating_ts_national_df.columns.intersection(
+        onshore_codes
+    )
+
+    return data_df[onshore_data_cols]
 
 
 def lookup_bus_pumas(data_ts_national_df, bus, bus_pumas):
@@ -120,11 +150,24 @@ def add_level_column(df, level_name="residential space"):
     return df
 
 
+# Implies that years are not duplicated each feature
+# & `scenario` column is present
+# TODO add sanity-checks
+def extract_growth_rates(scenario, growth_df, year, feature):
+    growth_rate = growth_rates_df.query("(year == @year) & feature == @feature")[
+        scenario
+    ]
+    return growth_rate
+
+
 if __name__ == "__main__":
     if "snakemake" not in globals():
         from _helpers_usa import mock_snakemake
 
         snakemake = mock_snakemake("aggregate_energyplus")
+
+    scenario = snakemake.params.thermal_scenario
+    heat_year = snakemake.params.thermal_proj_year
 
     default_path = pathlib.Path(__file__).parent.parent.parent
 
@@ -154,6 +197,7 @@ if __name__ == "__main__":
 
     heat_demand_path = pathlib.Path(default_path, snakemake.output.heat_demand_path[0])
     cool_demand_path = pathlib.Path(default_path, snakemake.output.cool_demand_path[0])
+    growth_rates_path = pathlib.Path(default_path, snakemake.input.growth_path)
 
     # gis information is processed for the model and PUMA regions
     # to match demand time series for the model bus regions
@@ -166,7 +210,21 @@ if __name__ == "__main__":
 
     puma_centroid = puma_gdf.copy()
     puma_centroid.geometry = puma_centroid.geometry.centroid
-    puma_centroid_merged = gpd.sjoin_nearest(puma_centroid, model_gdf, how="left")
+    puma_centroid_merged = gpd.sjoin_nearest(
+        puma_centroid, model_gdf, how="left", distance_col="distances"
+    )
+
+    growth_rates_df = pd.read_csv(growth_rates_path)
+
+    growth_rates_dict = {
+        x: extract_growth_rates(
+            scenario="reference case",
+            growth_df=growth_rates_df,
+            year=heat_year,
+            feature=x,
+        )
+        for x in THERM_LOAD_SECTORS
+    }
 
     # consolidating load profiles
     resstock_heating_ts_national_df = consolidate_pumas(
@@ -191,6 +249,42 @@ if __name__ == "__main__":
         data_path=state_comstock_cool_dir,
         states_abbr_df=states_abbr_df,
         puma_centroid_merged=puma_centroid_merged,
+    )
+
+    if FILTER_ISLAND_PUMAS:
+        # assuming that islands are not necessarily connected
+        # with the mainland power system
+        resstock_heating_ts_national_df = filter_by_island_pumas(
+            puma_centroid_merged, data_df=resstock_heating_ts_national_df
+        )
+        resstock_cooling_ts_national_df = filter_by_island_pumas(
+            puma_centroid_merged, data_df=resstock_cooling_ts_national_df
+        )
+
+        comstock_heating_ts_national_df = filter_by_island_pumas(
+            puma_centroid_merged, data_df=comstock_heating_ts_national_df
+        )
+        comstock_cooling_ts_national_df = filter_by_island_pumas(
+            puma_centroid_merged, data_df=comstock_cooling_ts_national_df
+        )
+
+    # Scaling is needed to account for the effect of scenarios
+    # on the thermal load
+    resstock_heating_ts_national_df = (
+        (growth_rates_dict["residential heating"].iloc[0])
+        * resstock_heating_ts_national_df
+    )
+    resstock_cooling_ts_national_df = (
+        (growth_rates_dict["residential cooling"].iloc[0])
+        * resstock_cooling_ts_national_df
+    )
+    comstock_heating_ts_national_df = (
+        (growth_rates_dict["services heating"].iloc[0])
+        * comstock_heating_ts_national_df
+    )
+    comstock_cooling_ts_national_df = (
+        (growth_rates_dict["services cooling"].iloc[0])
+        * comstock_cooling_ts_national_df
     )
 
     # time-series for each PUMA should be aggregated ------------------------------
@@ -280,6 +374,29 @@ if __name__ == "__main__":
             states_abbr_df=states_abbr_df,
             puma_centroid_merged=puma_centroid_merged,
         )
+
+        if FILTER_ISLAND_PUMAS:
+            # Assuming that islands are not nessecarily connected
+            # with the mainland power system
+            resstock_wrmwater_ts_national_df = filter_by_island_pumas(
+                puma_centroid_merged, data_df=resstock_wrmwater_ts_national_df
+            )
+            comstock_wrmwater_ts_national_df = filter_by_island_pumas(
+                puma_centroid_merged, data_df=comstock_wrmwater_ts_national_df
+            )
+
+        # Scaling is needed to account for the effect of scenarios
+        # on the thermal load
+        resstock_wrmwater_ts_national_df = (
+            (growth_rates_dict["residential water"].iloc[0])
+            * resstock_wrmwater_ts_national_df
+        )
+
+        comstock_wrmwater_ts_national_df = (
+            (growth_rates_dict["services water"].iloc[0])
+            * comstock_wrmwater_ts_national_df
+        )
+
         resstock_pumas_wrmwater_list = [None] * len(load_buses)
         comstock_pumas_wrmwater_list = [None] * len(load_buses)
 
